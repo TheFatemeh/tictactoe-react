4a28134cda65cf8c21919f56f58b8d086fd9628c
Seyedeh Fatemeh Ahmadi <SeyedehFatemehahmadi@outlook.com>
2021-12-12 21:45:26 +0330

    added turn label

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -72,7 +72,21 @@ export default class App extends Component {
 	};
 	
 	handleClose = () => {
-		this.setState({open: false})
+		this.setState({
+			blocks: [
+				{ id: 0, value: 0 },
+				{ id: 1, value: 0 },
+				{ id: 2, value: 0 },
+				{ id: 3, value: 0 },
+				{ id: 4, value: 0 },
+				{ id: 5, value: 0 },
+				{ id: 6, value: 0 },
+				{ id: 7, value: 0 },
+				{ id: 8, value: 0 },
+			],
+			turn: 0,
+			open: false
+		})
 	};
 
 	render() {
